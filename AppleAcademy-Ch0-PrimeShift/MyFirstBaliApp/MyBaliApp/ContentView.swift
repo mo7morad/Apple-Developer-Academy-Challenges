114d66aaf7a8f51ba9f2160c997ffa8c74e50f27
Moe <dronaessam@gmail.com>
2026-05-26 21:11:03 +0800

    fixing .xcodeproject issue

--- AppleAcademy-Ch0-PrimeShift/MyFirstBaliApp/MyBaliApp/ContentView.swift
+++ AppleAcademy-Ch0-PrimeShift/MyFirstBaliApp/MyBaliApp/ContentView.swift
@@ -9,43 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationStack {
-                    VStack(spacing: 20) {
-                        Text("Home Screen")
-                            .font(.largeTitle)
-                        
-                        // The Button
-                        NavigationLink(value: "Maintenance Details") {
-                            Text("Go to Maintenance")
-                                .padding()
-                                .background(Color.blue)
-                                .foregroundColor(.white)
-                                .cornerRadius(10)
-                        }
-                    }
-                    .navigationTitle("PrimeShift") // Adds a standard Apple header
-                    
-                    // The Map: Tells the stack what to do when it receives a String
-                    .navigationDestination(for: String.self) { destinationName in
-                        DetailView(title: destinationName)
-                    }
-                }
-        
+        Screen1View()
     }
 }
-struct DetailView: View {
-    let title: String
-    
-    var body: some View {
-        VStack {
-            Text("You navigated to:")
-            Text(title)
-                .font(.title)
-                .fontWeight(.bold)
-        }
-        // SwiftUI automatically gives this screen a "< Back" button!
-    }
-}
+
 #Preview {
     ContentView()
 }
--- AppleAcademy-Ch0-PrimeShift/MyFirstBaliApp/MyBaliApp/MyBaliApp.swift
+++ AppleAcademy-Ch0-PrimeShift/MyFirstBaliApp/MyBaliApp/MyBaliApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MyBaliAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Screen1View()
         }
     }
 }
